ab2a107de4a380720c6edbbcef457a96b86307fc
secure12 <trbhei@gmail.com>
2026-01-30 16:37:07 +0000

    add more to gmp_rename

--- bfibe/include/gmp_rename.h
+++ bfibe/include/gmp_rename.h
@@ -1,2 +1,26 @@
-#define __gmpz_realloc bfibe___gmpz_realloc
-#define __gmpz_set_ui bfibe___gmpz_set_ui
+#ifndef GMP_RENAMES_H
+#define GMP_RENAMES_H
+
+// mpz functions
+// clang-format off
+#define __gmpz_add          bfibe___gmpz_add
+#define __gmpz_add_ui       bfibe___gmpz_add_ui
+#define __gmpz_clear        bfibe___gmpz_clear
+#define __gmpz_clears       bfibe___gmpz_clears
+#define __gmpz_cmp          bfibe___gmpz_cmp
+#define __gmpz_get_str      bfibe___gmpz_get_str
+#define __gmpz_import       bfibe___gmpz_import
+#define __gmpz_init         bfibe___gmpz_init
+#define __gmpz_init_set     bfibe___gmpz_init_set
+#define __gmpz_inits        bfibe___gmpz_inits
+#define __gmpz_inp_str      bfibe___gmpz_inp_str
+#define __gmpz_mod          bfibe___gmpz_mod
+#define __gmpz_mul          bfibe___gmpz_mul
+#define __gmpz_out_str      bfibe___gmpz_out_str
+#define __gmpz_pow_ui       bfibe___gmpz_pow_ui
+#define __gmpz_set_str      bfibe___gmpz_set_str
+#define __gmpz_set_ui       bfibe___gmpz_set_ui
+#define __gmpz_sub_ui       bfibe___gmpz_sub_ui
+// clang-format on
+
+#endif
