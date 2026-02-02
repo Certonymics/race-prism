39d32de1e8cc8d6460eb047e292b3d17a1ef4ef5
secure12 <trbhei@gmail.com>
2026-02-02 11:54:10 +0000

    try disableing assembly

--- bfibe/cmake/FindGMP.cmake
+++ bfibe/cmake/FindGMP.cmake
@@ -26,6 +26,7 @@ if(IOS)
         CONFIGURE_COMMAND ./configure
             --prefix=${CMAKE_BINARY_DIR}/external/gmp
             --host=aarch64-apple-darwin
+            --disable-assembly
             CC=${CMAKE_C_COMPILER}
             "CFLAGS=-arch arm64 -isysroot ${CMAKE_OSX_SYSROOT} -mios-version-min=12.0 -include ${CMAKE_CURRENT_SOURCE_DIR}/include/gmp_rename.h"
         BUILD_COMMAND make
@@ -46,6 +47,7 @@ elseif(ANDROID)
         CONFIGURE_COMMAND ./configure
             --prefix=${CMAKE_BINARY_DIR}/external/gmp
             --host=aarch64-linux-android
+            --disable-assembly
             CC=${CMAKE_C_COMPILER}
         BUILD_COMMAND make
         BUILD_IN_SOURCE 1
